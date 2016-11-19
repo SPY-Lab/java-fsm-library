cca1f4910bfa5b0bfd04a5479e0620c71e790088
VincenzoArceri <vincenzoarceri.92@gmail.com>
2016-11-19 17:46:49 +0100

    New branch

--- src/it/univr/main/Main.java
+++ src/it/univr/main/Main.java
@@ -3,6 +3,13 @@ package it.univr.main;
 import it.univr.equations.*;
 import it.univr.machine.*;
 
+/**
+ * Main class.
+ * 
+ * @author <a href="mailto:vincenzo.arceri@univr.it">Vincenzo Arceri</a>
+ * @version 0.1
+ * @since 19-11-2016
+ */
 public class Main {
 
 	public static void main(String[] args) {
