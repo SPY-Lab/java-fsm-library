98643b05f3dc38dd60634155a57d79946ee1b420
Andrea Perazzoli <andreaperazzoli@MacBook-Pro-di-Andrea.local>
2016-11-21 17:09:50 +0100

    first commit

--- src/it/univr/fsm/main/Main.java
+++ src/it/univr/fsm/main/Main.java
@@ -13,7 +13,7 @@ import it.univr.fsm.machine.*;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(Automaton.makeAutomaton("'helloworld'"));
+		System.out.println(Automaton.makeAutomaton("'ciao'"));
 	}
 
 }
